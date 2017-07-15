460b7bf86dffb45a3f142cabd9fdf68285fd4b35
bharadwaj725 <bharadwaj.juvva@gmail.com>
2017-07-15 21:06:46 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-# Copyright 2013 Google Inc. All Rights Reserved.
+# Copyright 2016 Google Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#         http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,68 +12,22 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""
-Sample App Engine application demonstrating how to connect to Google Cloud SQL
-using App Engine's native unix socket or using TCP when running locally.
+# [START app]
+import logging
 
-For more information, see the README.md.
-"""
+# [START imports]
+from flask import Flask, render_template, request
+# [END imports]
 
-# [START all]
-import os
-
-import MySQLdb
-import webapp2
+# [START create_app]
+app = Flask(__name__)
+# [END create_app]
 
 
-# These environment variables are configured in app.yaml.
-CLOUDSQL_CONNECTION_NAME = os.environ.get('CLOUDSQL_CONNECTION_NAME')
-CLOUDSQL_USER = os.environ.get('CLOUDSQL_USER')
-CLOUDSQL_PASSWORD = os.environ.get('CLOUDSQL_PASSWORD')
+# [START form]
+@app.route('/form')
+def form():
+    return render_template('form.html')
+# [END form]
 
 
-def connect_to_cloudsql():
-    # When deployed to App Engine, the `SERVER_SOFTWARE` environment variable
-    # will be set to 'Google App Engine/version'.
-    if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
-        # Connect using the unix socket located at
-        # /cloudsql/cloudsql-connection-name.
-        cloudsql_unix_socket = os.path.join(
-            '/cloudsql', CLOUDSQL_CONNECTION_NAME)
-
-        db = MySQLdb.connect(
-            unix_socket=cloudsql_unix_socket,
-            user=CLOUDSQL_USER,
-            passwd=CLOUDSQL_PASSWORD)
-
-    # If the unix socket is unavailable, then try to connect using TCP. This
-    # will work if you're running a local MySQL server or using the Cloud SQL
-    # proxy, for example:
-    #
-    #   $ cloud_sql_proxy -instances=your-connection-name=tcp:3306
-    #
-    else:
-        db = MySQLdb.connect(
-            host='127.0.0.1', user=CLOUDSQL_USER, passwd=CLOUDSQL_PASSWORD)
-
-    return db
-
-
-class MainPage(webapp2.RequestHandler):
-    def get(self):
-        """Simple request handler that shows all of the MySQL variables."""
-        self.response.headers['Content-Type'] = 'text/plain'
-
-        db = connect_to_cloudsql()
-        cursor = db.cursor()
-        cursor.execute('SHOW VARIABLES')
-
-        for r in cursor.fetchall():
-            self.response.write('{}\n'.format(r))
-
-
-app = webapp2.WSGIApplication([
-    ('/', MainPage),
-], debug=True)
-
-# [END all]
